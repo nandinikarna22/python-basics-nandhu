129abd5296a016ac127391d4c4acf23bc50142a0
nandinikarna22 <nandinikarna133@gmail.com>
2025-07-30 13:46:36 +0530

    uploading app1.py

---
+++ app1.py
@@ -0,0 +1,13 @@
+name="MLEW"
+number=10.0
+isOn=True
+letter='H'
+#Arithemetic- + - * / %
+#logical- and,or,not
+#comparision- <,>,==,!=,<=,>=
+#assignment - =,+=,-=,*=,/=
+if (number>5):
+    print(name)
+else:
+    print(isOn)
+    
